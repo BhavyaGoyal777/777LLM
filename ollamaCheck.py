423bbbf156aa3dbf6d6a1b98a1b216b6c53ba547
Bhavya Goyal <cubersfromindia@gmail.com>
2025-02-24 21:37:59 +0530

    ollama check

---
+++ ollamaCheck.py
@@ -0,0 +1,17 @@
+import psutil
+
+def check_if_running(process_name):
+    running = False
+    for proc in psutil.process_iter(['name']):
+        if process_name in proc.info['name']:
+            running = True
+            break
+    return running
+
+ollama_running = check_if_running("ollama")
+
+if not ollama_running:
+    raise RuntimeError(
+        "Ollama not running"
+    )
+print("Ollama is running:",check_if_running("ollama"))
